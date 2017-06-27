dff6ad732908adb4be377803b1f88c9783e7a9a9
simingl <bryan.liusiming@gmail.com>
2017-06-27 11:41:13 -0700

    init scene
    clean version

---
+++ Assets/scripts/ROSBridgeLib/DroneImageSensor.cs
@@ -0,0 +1,38 @@
+﻿using ROSBridgeLib;
+using ROSBridgeLib.sensor_msgs;
+using ROSBridgeLib.std_msgs;
+using ROSBridgeLib.turtlesim;
+using System.Collections;
+using SimpleJSON;
+using UnityEngine;
+
+/**
+ * This is a toy example of the Unity-ROS interface talking to the TurtleSim 
+ * tutorial (circa Groovy). Note that due to some changes since then this will have
+ * to be slightly re-written, but as its a test ....
+ * 
+ * This defines the callback that links the color_sensor message and its callback
+ * 
+ * @author Michael Jenkin, Robert Codd-Downey and Andrew Speers
+ * @version 3.0
+ **/
+
+public class DroneImageSensor : ROSBridgeSubscriber {
+	
+	public new static string GetMessageTopic() {
+		return "/quad/camera/image_raw/compressed";
+    }  
+	
+	public new static string GetMessageType() {
+		return "sensor_msgs/CompressedImage";
+    }
+	
+	public new static ROSBridgeMsg ParseMessage(JSONNode msg) {
+        return new CompressedImageMsg(msg);
+    }
+	
+	public new static void CallBack(ROSBridgeMsg msg) {
+        CompressedImageMsg imgMsg = msg as CompressedImageMsg;
+        //HUD.droneCamera.LoadImage(imgMsg.GetImage());
+    }
+}
---
+++ Assets/scripts/ROSBridgeLib/ROSManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using ROSBridgeLib;
+using ROSBridgeLib.geometry_msgs;
+
+public class ROSManager{
+	private static ROSManager instance = new ROSManager();
+
+    private ROSBridgeWebSocketConnection ros = null;
+    private Boolean lineOn;
+
+    public static ROSManager getInstance(){
+		return instance;
+	}
+
+	private ROSManager(){
+        ROSConnect();
+	}
+
+    public void ROSConnect() {
+        ros = new ROSBridgeWebSocketConnection("ws://134.197.87.18", 9090);
+        ros.AddSubscriber(typeof(RobotImageSensor));
+        ros.AddSubscriber(typeof(DroneImageSensor));
+        ros.AddPublisher(typeof(RobotTeleop));
+        ros.Connect();
+        lineOn = true;
+    }
+
+    public void ROSRender() {
+        this.ros.Render();
+    }
+    public void RemoteControl(Vector3 linear, Vector3 angular) { 
+        TwistMsg msg = new TwistMsg (new Vector3Msg(linear.x, linear.y, linear.z), new Vector3Msg(angular.x, angular.y, angular.z));
+        ros.Publish (RobotTeleop.GetMessageTopic (), msg);
+    }
+
+    public void ROSDisconnect()
+    {
+        if (ros != null)
+            ros.Disconnect();
+    }
+
+
+}
