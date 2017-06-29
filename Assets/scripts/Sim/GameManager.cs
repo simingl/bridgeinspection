2969f21a43b2163775ca1ae814e4a4f6f2c57ed9
simingl <bryan.liusiming@gmail.com>
2017-06-29 10:58:58 -0700

    Add UBD Control

---
+++ Assets/scripts/Sim/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private ROSManager rosManager;
+    private UIManager uiManager;
+
+	// Use this for initialization
+	void Start () {
+        rosManager = ROSManager.getInstance();
+        //uiManager = new UIManager();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/Sim/GameObject/UBDControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UBDControl : MonoBehaviour {
+    private float speed = 0f;
+    private float minSpeed = 0f;
+    private float maxSpeed = 2f;
+    private float moveSpeed, rotateSpeed;
+    private float acceleration = 0.3f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.HandleKeyboardControl();
+    }
+
+    private void HandleKeyboardControl()
+    {
+        //rotating
+        Vector3 leftaxis = transform.TransformDirection(Vector3.up);
+        this.transform.RotateAround(transform.position, leftaxis, Input.GetAxis("Horizontal"));
+        //moving forward and backward
+        this.transform.Translate(Input.GetAxis("Vertical") * Vector3.left * Time.deltaTime);
+
+        //constructing ROS teleop message
+        //float _dx = Input.GetAxis("Horizontal");
+        //float _dy = Input.GetAxis("Vertical");
+        //float linear = _dy * 0.5f;
+        //float angular = -_dx * 0.2f;
+        //ROSManager.getInstance().RemoteControl(new Vector3(linear, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, angular));
+    }
+
+
+}
