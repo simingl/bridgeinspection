0e1d0cbfc0642ad2e6beb0c69b57bd05a74546e9
Robert Lowry <rjlowry80@users.noreply.github.com>
2017-07-05 16:24:54 -0700

    Worked on Menu and HeadlightToggle.

---
+++ Assets/ToggleHeadlights.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ToggleHeadlights : MonoBehaviour {
+
+    public Light HeadLightR;
+    public Light HeadLightL;
+    public Toggle headlights;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleInput( bool newValue )
+    {
+
+    }
+}
