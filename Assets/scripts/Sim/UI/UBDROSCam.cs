f71872c5ca56ef622df4d1a655957c12df4538fd
Robert Lowry <rjlowry80@users.noreply.github.com>
2017-07-18 10:40:14 -0700

    Working on UBDRosCam

---
+++ Assets/scripts/Sim/UI/UBDROSCam.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UBDROSCam : MonoBehaviour {
+    private GameManager gameManager;
+    private ROSManager rosManager;
+
+    private RawImage rawImage;
+
+    // Use this for initialization
+    void Start () {
+        gameManager = GameManager.getInstance();
+        rosManager = gameManager.getROSManager();
+
+        rawImage = this.gameObject.GetComponent<RawImage>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    private void OnGUI()
+    {
+        rawImage.texture = rosManager.getUBDCam();
+    }
+
+    public void ToggleUBDCam(bool toggle)
+    {
+        if (rawImage.enabled)
+        {
+            rawImage.enabled = false;
+        }
+        else
+        {
+            rawImage.enabled = true;
+        }
+    }
+}
