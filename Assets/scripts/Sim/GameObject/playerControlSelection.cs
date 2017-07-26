0f3fddd251e24103f7224e7607961bbe168537fb
Robert Lowry <rjlowry80@users.noreply.github.com>
2017-07-26 15:19:25 -0700

    Controller changes with view change.

---
+++ Assets/scripts/Sim/GameObject/playerControlSelection.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControlSelection : MonoBehaviour {
+
+    public GameObject[] playerSelect;
+
+    GameObject currentPlayer;
+   
+    private int controlIndex;
+
+    //used for reference to the camera position
+    public camView positionReference;
+
+    // Use this for initialization
+    void Start () {
+
+        enableCurrentPlayerMovementControl();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void getCurrentPlayerObject()
+    {
+        controlIndex = positionReference.getViewPositionInt();
+
+        currentPlayer = playerSelect[controlIndex - 1];
+    }
+
+    private void disableAllPlayerControls()
+    {
+        for (int i = 0; i < playerSelect.Length; i++)
+        {
+            playerSelect[i].GetComponent<movementControl>().enabled = false;
+        }
+    }
+
+    public void enableCurrentPlayerMovementControl()
+    {
+        disableAllPlayerControls();
+
+        getCurrentPlayerObject();
+
+        currentPlayer.GetComponent<movementControl>().enabled = true;
+
+    }
+}
