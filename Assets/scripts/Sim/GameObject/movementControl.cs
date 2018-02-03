21e6888ab5912a5eeb98a9a01b84d76586cd0bd1
Robert Lowry <rjlowry80@users.noreply.github.com>
2018-02-02 16:24:01 -0800

    Trying to use spherecast for magnetic physics.

--- Assets/scripts/Sim/GameObject/movementControl.cs
+++ Assets/scripts/Sim/GameObject/movementControl.cs
@@ -24,10 +24,37 @@ public class movementControl : MonoBehaviour {
         {
             ubdMovement();
         }
+        else if(this.gameObject.name == "myRobot2")
+        {
+            robotMovement();
+        }
         else
         {
             flyingMovement();
         }
+    }
+
+    private void robotMovement()
+    {
+        //float modifySpeed = 0.15f;
+
+        //Angular movement
+        //get axis to rotate around
+        Vector3 leftaxis = transform.TransformDirection(Vector3.up);
+        //rotate
+        this.transform.RotateAround(transform.position, leftaxis, Input.GetAxis("Horizontal"));
+
+        //Linear movement
+        //Vector3.left is used because UBD transform is off 
+        this.transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * modifySpeed * Time.deltaTime);
+
+        //constructing ROS teleop message
+        //float _dx = Input.GetAxis("Horizontal");
+        //float _dy = Input.GetAxis("Vertical");
+        //float linear = _dy * 0.6f;
+        //float angular = -_dx * 1.6f;
+        //ROSManager.getInstance().RemoteControl(new Vector3(linear, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, angular));
+
     }
 
 
---
+++ Assets/scripts/Sim/physics/crawlerWheelSpherecast.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crawlerWheelSpherecast : MonoBehaviour {
+
+    int magneticLayerMask = 1 << 13;
+    private Rigidbody rb;
+    private Transform tf;
+
+    
+    public float sc_radius = 1.0f;
+    public float sc_distance = 0.5f;
+    public float adjustMagneticForce = 0.2f;
+
+    private Vector3 direction;
+    private Vector3 origin;
+    //private Vector3 down;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+        tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        RaycastHit hit;
+        origin = tf.position;
+        direction = tf.position;
+        //down = -transform.right;
+
+
+
+        if (Physics.SphereCast(origin, sc_radius, direction, out hit, sc_distance, magneticLayerMask))
+        {
+            Debug.DrawLine(origin, hit.point, Color.magenta);
+            Debug.Log("Spherecast hit magnetic object.");
+
+            rb.AddForce(tf.position - hit.point * adjustMagneticForce);
+            Debug.DrawRay(tf.position, tf.position - hit.point, Color.blue);
+
+        }
+	}
+
+    /*
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(origin, sc_radius);
+    }
+    */
+}
